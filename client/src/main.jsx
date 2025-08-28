69e7774848f065f2440be3932397e5b88bc034ab
Ayan husain03 <hussainayan9897@gmail.com>
2025-08-28 20:57:33 +0530

    feat: integrate Redux Persist for state persistence and enhance store configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,16 @@
-
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <ToastContainer />
-    <App />
+    <PersistGate loading={<div>loading.....</div>} persistor={persistor}>
+      <ToastContainer />
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import sessionStorage from "redux-persist/es/storage/session";
+import { combineReducers } from "@reduxjs/toolkit";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+  user: authReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage: sessionStorage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: authReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
